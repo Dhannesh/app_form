b953596230aa6e90cc8b29a3c3d6a69143f61f62
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-02-28 07:47:07 +0530

    add radio widget

---
+++ lib/RadioStore.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class Radiostore extends StatelessWidget {
+  const Radiostore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Insta Store",
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Insta Store"),
+        ),
+        body: const RadioInputWidget(),
+      ),
+    );
+  }
+}
+
+enum Size{
+  small,
+  medium,
+  large
+}
+
+class RadioInputWidget extends StatefulWidget {
+  const RadioInputWidget({super.key});
+
+  @override
+  State<RadioInputWidget> createState() => _RadioInputWidgetState();
+}
+
+class _RadioInputWidgetState extends State<RadioInputWidget> {
+  Size _size = Size.large;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: 300,
+            padding: const EdgeInsets.all(40),
+            margin: const EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.orangeAccent, width: 2),
+              image: const DecorationImage(
+                image: AssetImage("images/heels.jpg"),
+                fit: BoxFit.cover
+              ),
+              shape: BoxShape.circle
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: Text(
+                "Currently selected: $_size",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 22,
+                  color:Colors.orange,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+
+          ),
+          ListTile(
+            title: const Text("Small"),
+            leading: Radio<Size>(
+              value: Size.small,
+              groupValue: _size,
+              onChanged: (Size? value){
+                setState(() {
+                  _size = value!;
+                });
+              }
+          ),
+          trailing: const Text("6 UK"),
+          ),
+          ListTile(
+            title: const Text("Medium"),
+            leading: Radio<Size>(
+                value: Size.medium,
+                groupValue: _size,
+                onChanged: (Size? value){
+                  setState(() {
+                    _size = value!;
+                  });
+                }
+            ),
+            trailing: const Text("8 UK"),
+          ),
+          ListTile(
+            title: const Text("Large"),
+            leading: Radio<Size>(
+                value: Size.large,
+                groupValue: _size,
+                onChanged: (Size? value){
+                  setState(() {
+                    _size = value!;
+                  });
+                }
+          ),
+            trailing: const Text("10 UK"),
+          )
+        ],
+      ),
+
+    );
+  }
+}
+
---
+++ lib/TextFieldStore.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class TextFieldStore extends StatelessWidget {
+  const TextFieldStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Insta Store",
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Insta Store"),
+        ),
+        body: const TextInputWidget(),
+      ),
+    );
+  }
+}
+
+class TextInputWidget extends StatefulWidget {
+  const TextInputWidget({super.key});
+
+  @override
+  State<TextInputWidget> createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  String thoughts = "No thoughts yet...";
+  final _textController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _textController.addListener(() {
+      final String text = _textController.text.toUpperCase();
+      _textController.value = _textController.value.copyWith(
+          text: text,
+          selection: TextSelection(
+              baseOffset: text.length, extentOffset: text.length));
+      setState(() {
+        thoughts = text;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(children: <Widget>[
+        TextField(
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.message),
+              border: OutlineInputBorder(),
+              labelText: "Share your thoughts: ",
+              hintText: "What's on your mind?"),
+          keyboardType: TextInputType.text,
+          maxLength: 25,
+          controller: _textController,
+        ),
+        Center(
+            child: Container(
+          padding: const EdgeInsets.all(12),
+          child: Text(
+            thoughts,
+            style: const TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 25,
+            ),
+          ),
+        ))
+      ]),
+    );
+  }
+}
